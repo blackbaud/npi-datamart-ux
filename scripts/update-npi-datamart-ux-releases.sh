ec046da8981e51bebec88e9176945887a5ea4b18
Corey Archer <Corey.Archer@blackbaud.me>
2016-06-02 13:12:52 -0400

    Release v0.2.0

--- scripts/update-npi-datamart-ux-releases.sh
+++ scripts/update-npi-datamart-ux-releases.sh
@@ -3,35 +3,34 @@ set -e
 
 # Only run for a release
 if [[ "$IS_RELEASE" == "true" ]]; then
-  echo -e "Starting to update npi-datamart-ux-releases.\n"
+  echo -e "Starting to update npi-datamart-ux releases.\n"
 
   # What user will be committing to the sky-docs repo
   git config --global user.email "sky-build-user@blackbaud.com"
   git config --global user.name "Blackbaud Sky Build User"
-
-  # Clones the npi-datamart-ux-docs repo into a "gh/" folder
-  git clone --quiet https://${GH_TOKEN}@github.com/blackbaud/npi-datamart-ux.git gh > /dev/null
+  
+  git clone --quiet https://${GH_TOKEN}@github.com/blackbaud/npi-datamart-ux.git npiux > /dev/null
 
   # Verify the version doesn't already exist
-  if [[ -d "gh/releases/npiux/$RELEASE_VERSION" ]]; then
+  if [[ -d "npiux/releases/$RELEASE_VERSION" ]]; then
     echo "npi-datamart-ux-releases already contains version $RELEASE_VERSION"
     exit 1
   else
 
     # Copy the dist folder
-    cp -rf dist/. gh/releases/skyux/$RELEASE_VERSION/
+    cp -rf dist/. npiux/releases/$RELEASE_VERSION/
 
     # Commit and push all our changes to the repo
-    cd gh
+    cd npiux
     git add -f .
-    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed $RELEASE_VERSION to skyux-releases"
+    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed $RELEASE_VERSION to npi-datamart-ux releases"
     git push -fq origin master > /dev/null
 
-    echo -e "npi-datamrt-ux-releases successfully updated.\n"
+    echo -e "npi-datamrt-ux releases successfully updated.\n"
 
     # Publish to NPM
     cd ../
-    echo -e "blackbaud-skyux\n$NPM_PASSWORD\nsky-build-user@blackbaud.com" | npm login
+    echo -e "blackbaud-npi-datamart-ux\n$NPM_PASSWORD\nsky-build-user@blackbaud.com" | npm login
     npm whoami
     npm publish
     echo -e "npi-datamart-ux successfully deployed to NPM.\n"
