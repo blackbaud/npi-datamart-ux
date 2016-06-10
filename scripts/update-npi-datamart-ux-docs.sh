c9686d474a22d44a066ab5acd3ed8c9d11f66768
Blackbaud-CoreyArcher <corey.archer@blackbaud.com>
2016-06-10 11:38:37 -0400

    fixed docs script

--- scripts/update-npi-datamart-ux-docs.sh
+++ scripts/update-npi-datamart-ux-docs.sh
@@ -9,6 +9,9 @@ branch="carcher"
 # Clones the npi-datamart repo into a "docs/" folder
 git clone --quiet --branch=$branch https://${GH_TOKEN}@github.com/blackbaud/npi-datamart.git docs > /dev/null
 
+# Copy jsdoc output
+cp -rf stache/. docs/content/npi-datamart-ux
+
 # What user will be committing to the sky-docs repo
 git config --global user.email "sky-build-user@blackbaud.com"
 git config --global user.name "Blackbaud Sky Build User"
@@ -16,11 +19,21 @@ git config --global user.name "Blackbaud Sky Build User"
 # Commit and push all our changes to the repo
 cd docs
 git add -f .
+if [ -z "$(git status --porcelain)" ]; then
+  echo -e "No changes to commit to npi-datamart."
+else
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to npi-datamart"
+  git push -fq origin $branch > /dev/null
+  echo -e "npi-datamart successfully updated.\n"
+
+fi
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed $RELEASE_VERSION to npi-datamart-ux docs"
 git push -fq origin $branch > /dev/null
 
 # Only run for a release
 if [[ "$IS_RELEASE" == "true" && "$IS_PRERELEASE" == "false" ]]; then
+
+  echo -e "Starting to update npi-datamart stache site\n"
 
   cd ..
 
@@ -28,7 +41,6 @@ if [[ "$IS_RELEASE" == "true" && "$IS_PRERELEASE" == "false" ]]; then
   git clone --quiet --branch=master http://npi-datamart.scm.docs.blackbaudhosting.com/npi-datamart.git web > /dev/null
 
   # Copy jsdoc output
-  cp -rf stache/. docs/content/npi-datamart-ux
   cp -rf stache/. web/content/npi-datamart-ux
 
   # Copy integrity hashes
@@ -36,9 +48,12 @@ if [[ "$IS_RELEASE" == "true" && "$IS_PRERELEASE" == "false" ]]; then
 
   cd web
   git add -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed $RELEASE_VERSION to npi-datamart-ux docs"
-  git push -fq website master > /dev/null
-
+  if [ -z "$(git status --porcelain)" ]; then
+    echo -e "No changes to commit to npi-datamart stache."
+  else
+    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed $RELEASE_VERSION to npi-datamart stache"
+    git push -fq website master > /dev/null
+  fi
 fi
   
 echo -e "NPI Datamart UX Docs successfully updated.\n"
