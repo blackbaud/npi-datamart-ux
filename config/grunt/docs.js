f82ad8af7838420f2df52f7e55cde4a2f1125fc1
Blackbaud-CoreyArcher <corey.archer@blackbaud.com>
2016-06-14 11:33:23 -0400

    adding showcase icons

--- config/grunt/docs.js
+++ config/grunt/docs.js
@@ -45,28 +45,41 @@ module.exports = function (grunt, env, utils) {
             order = 10;
         
         grunt.file.expand(options, '*/docs/*.md').forEach(function (filename) {
-            var content,
-                component,
-                frontmatter,
-                pathFrontmatter,
+            var component,
                 pathMarkdown,
+                content,
                 lines,
+                pathFrontmatter,
+                frontmatter,
+                pathIcons,
+                icons,
+                i,
                 newFile;
             
             component = filename.substr(0, filename.indexOf('/'));
             pathMarkdown = grunt.config.get('npiux.paths.src') + filename;
-            pathFrontmatter = grunt.config.get('npiux.paths.src') + 'docs-header.tmpl';
-            frontmatter = grunt.file.read(pathFrontmatter);
             content = grunt.file.read(pathMarkdown).replace(/\r\n/g, '\n'); // Standardize Reports MD file
             lines = content.split(/[\r\n]/); // Find stray CR character
+            pathFrontmatter = grunt.config.get('npiux.paths.src') + 'docs-header.tmpl';
+            frontmatter = grunt.file.read(pathFrontmatter);
+            pathIcons = grunt.config.get('npiux.paths.src') + 'docs-icons.tmpl';
+            icons = grunt.file.read(pathIcons).replace(/\r\n/g, '\n').split('\n');
+            for (i = 0; i < icons.length; i++) {
+                if (icons[i].includes(component)) {
+                    frontmatter = frontmatter.replace('<<desc>>', '<<desc>>\nicon' + icons[i].substr(icons[i].indexOf(':')));
+                    break;
+                }
+            }
             frontmatter = frontmatter.replace('<<order>>', order);
             order += 10;
             if (order > 30) { // Don't modify Reports MD
                 frontmatter = frontmatter.replace('<<component>>', lines[2].substr(2));
                 frontmatter = frontmatter.replace('<<desc>>', lines[3]);
+                frontmatter = frontmatter.replace('<<icon>>', lines[4]);
             } else {
                 frontmatter = frontmatter.replace('<<component>>', lines[3]);
                 frontmatter = frontmatter.replace('<<desc>>', lines[4]);
+                frontmatter = frontmatter.replace('<<icon>>', lines[5]);
                 lines[3] = '# ' + lines[3];
                 lines.splice(2, 1);
                 content = lines.join('\n');
