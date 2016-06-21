2113f7983b69abb93acf96bf0c8429654979c93e
Blackbaud Sky Build User <sky-build-user@blackbaud.com>
2016-06-21 20:52:41 +0000

    Travis build 96 pushed to npi-datamart-ux [ci skip]

--- dist/js/npi-datamart.js
+++ dist/js/npi-datamart.js
@@ -3,11 +3,11 @@
 
 (function () {
     'use strict';
-    //YAML Title, YAML Description, JSDoc Heading
+    //JSDoc Module Heading, YAML Title, YAML Description
     /**
-     * Data Mart API
-     * API module for NPI Data Mart UX
      * @module npi-datamart.api
+     * @title Data Mart API
+     * @description API module for NPI Data Mart UX
      */
     angular.module('npi-datamart.api', ['npi-datamart.authentication'])
         .factory('BBDataMartAPI', ['$q', '$timeout', '$http', function ($q, $timeout, $http) {
@@ -620,11 +620,11 @@
 
 (function () {
     'use strict';
-    //YAML Title, YAML Description, JSDoc Heading
+    //JSDoc Module Heading, YAML Title, YAML Description
     /**
-     * Data Mart Authentication
-     * Module for handling Single Sign-On
      * @module npi-datamart.authentication
+     * @title Data Mart Authentication
+     * @description Module for handling Single Sign-On
      */
     angular.module('npi-datamart.authentication', [])
         .factory('BBDataMartAuthentication', ['$q', '$http', '$rootScope', function ($q, $http, $rootScope) {
@@ -632,11 +632,11 @@
              * An Angular Factory to create BBDataMartAuthentication objects
              * @method BBDataMartAuthentication
              * @param {Object} options Object containing the information for domain and single sign on
-             * @param {string} options.domain The domain
-             * @param {Function} options.getDomain A promise returning the domain
-             * @param {string} options.ssoProvider the SSO provider
-             * @param {Function} options.getSSOProvider A promise returning the SSO provider
-             * @param {Function} options.getSSOToken A promise returning the SSO token
+             * @param {string} [options.domain] The domain.
+             * @param {Function} [options.getDomain] A promise returning the domain. Required if options.domain is not specified.
+             * @param {string} [options.ssoProvider] the SSO provider.
+             * @param {Function} [options.getSSOProvider] A promise returning the SSO provider. Required if options.ssoProvider is not specified.
+             * @param {Function} options.getSSOToken A promise returning the SSO token.
              * @return {BBDataMartAuthentication} The class containing methods to handle authentication on the Data Mart API
              */
             var BBDataMartAuthentication = function (options) {
@@ -903,13 +903,21 @@
             "extraFonts": { 'googlefonts': ['Oswald', 'Open Sans'] }
         }
     };
-    //YAML Title, YAML Description, JSDoc Heading
+    //JSDoc Module Heading, YAML Title, YAML Description
     /**
-     * Data Mart Report
-     * Authentication module for NPI Data Mart UX  
      * @module npi-datamart.report
+     * @title Data Mart Report
+     * @description Module for embedding dashboards and reports on a page
      */
     angular.module('npi-datamart.report', ['npi-datamart.templates', 'npi-datamart.api', 'sky'])
+        /**
+         * @name bbDataMartReportConfiguration
+         * @description Constant used to configure application-wide behavior for the directives in the  `npi-datamart.report` module. Some options must be specified in order to use the directives, while others are optional.
+         * @param {Object} api Must be set to an instance of `BBDataMartAPI` that directives should use to work with the Data Mart API in order to authenticate and display reports and dashboards.
+         * @param {function} [linkHandler] A function that will handle links clicked in a report or dashboard. When a link is clicked, the function will be called and provided the link's targe URL.
+         * @param {function} [drillHandler] A function that will handle drill operations in a report or dashboard. When a drill event is triggered, the function will be called and provided the drill context from the event.
+         * @param {Object} [processFilters] Hook for pre-processing filters provided to a report or dashboard directive. This can be used to have more product-friendly naming of filters within the product code that supplies filters to the directives.  This hook allows these filters to be translated into the names expected by the data mart project before being used.
+         */
         .constant('bbDataMartReportConfiguration', {
             api: null, //Set to an instance of BBDataMartAPI to use by default for all directives it not otherwise specified                        
             linkHandler: null, //Optionally set to function to handle when links are clicked in the dashboards and reports
@@ -1132,8 +1140,14 @@
             };
         }])
         /**
-         * The Data Mart Report directive
          * @name bbDataMartReport
+         * @description Directive for displaying a single data mart report on a page. When loaded, the directive will authenticate with the Data Mart API (if not already authenticated) and ensure authentication is maintained until the directive is destroyed. It will show the specified report as an embedded iFrame on the page.
+         * @param {directive} bb-data-mart-report Displays a report as an embedded iFrame.
+         * @param {directive} bb-data-mart-report.bb-data-mart-report-id The report ID of the report to be displayed.
+         * @param {directive} [bb-data-mart-report.bb-data-mart-report-filters] An object describing filters to be applied to the report using the querystring URL filters feature of reports. The keys and values of this object will be applied as filters.  The `bbDataMartReportConfiguration.processFilters` function, if defined, will be executed on the filters object before it is used.
+         * @param {directive} [bb-data-mart-report.bb-data-mart-report-drill-header] Overrides the `bbDataMartReportConfiguration.processFilters` function for a specific directive.
+         * @param {directive} [bb-data-mart-report.height] Sets the height attribute of the iFrame.
+         * @param {directive} [bb-data-mart-report.width] Sets the width attribute of the iFrame.
          */
         .directive('bbDataMartReport', ['bbDataMartReportService', function (bbDataMartReportService) {
             return {
@@ -1152,8 +1166,16 @@
             };
         }])
         /**
-         * The Data Mart Dashboard directive
+         * Directive for displaying a report dashboard on a page. When loaded, the directive will authenticate with the Data Mart API (if not already authenticated) and ensure authentication is maintained until the directive is destroyed. It will show the specified dashboard as an embedded iFrame on the page. The height of the iFrame will be set automatically based on the height of the dashboard.
+         * The embedded dashboard will have Sky NPI CSS styles injected to use a Sky look and feel.
+         * The directive also supports embedding help links directly in the dashboards. Clicking these links will cause the Help flyout to open to a specified topic. To embed a help link, use the "Add Web Content" feature in the dashboard designer.  For the url, use `https://www.blackbaud.com/files/support/helpfiles/npi/npi_help.html?helpkey=<<INSERT HELP KEY>>.html`. 
          * @name bbDataMartDashboard
+         * @param {directive} bb-data-mart-dashboard
+         * @param {directive} bb-data-mart-dashboard.bb-data-mart-dashboard-id The report ID of the dashboard to be displayed.
+         * @param {directive} [bb-data-mart-dashboard.bb-data-mart-dashboard-filters] An object describing filters to be applied to the dashboard using the querystring URL filters feature of dahboards. The keys and values of this object will be applied as filters.  The `bbDataMartReportConfiguration.processFilters` function, if defined, will be executed on the filters object before it is used.
+         * @param {directive} [bb-data-mart-dashboard.bb-data-mart-dashboard-drill-handler] Overrides the `bbDataMartReportConfiguration.processFilters` function for a specific directive.
+         * @param {directive} [bb-data-mart-dashboard.bb-data-mart-dashboard-no-chrome] If true, does not include the dashboard chrome for saving filters and exporting as PDF.
+         * @param {directive} [bb-data-mart-dashboard.width] Sets the width attribute of the iFrame.
          */
         .directive('bbDataMartDashboard', ['bbDataMartReportService', function (bbDataMartReportService) {
             return {
@@ -1172,8 +1194,14 @@
             };
         }])
         /**
-         * The directive that handles dynamic page events
+         * Directive for creating the effect of reponsive design for dashboards. Data Mart dashboards currently have a fixed width design. This directive provides responsive design by allowing different dashboards to be used at different breakpoints.  A collection of dashboards can be designed to show the same or similar reports in different layouts for the different breakpoints, and this directive will display the correct dashboard based on the size of the device.  If the device width changes (for example, due to changing orientation) the dashboard can change as well.
+         * Note that when using a responsive dashboard, the `bb-data-mart-dashboard-no-chrome` option is used to hide the chrome when not viewing on a large (desktop) device.
          * @name bbDataMartResponsiveDashboard
+         * @param {directive} bb-data-mart-responsive-dashboard
+         * @param {directive} [bb-data-mart-responsive-dashboard.bb-data-mart-responsive-dashboard-xs The dashboard id of the dashboard to display on extra small devices (phone).
+         * @param {directive} [bb-data-mart-responsive-dashboard.bb-data-mart-responsive-dashboard-sm The dashboard id of the dashboard to display on small devices (portait tablets).
+         * @param {directive} [bb-data-mart-responsive-dashboard.bb-data-mart-responsive-dashboard-lg The dashboard id of the dashboard to display on medium (landscape tablets) and large devices (desktop).  There is no distinction between medium and large devices because the maximum size of a dashboard already fits on the medium device width.
+         * @param {directive} [bb-data-mart-responsive-dashboard.bb-data-mart-responsive-dashboard-drill-handler] Overrides the `bbDataMartReportConfiguration.processFilters` function for a specific directive. 
          */
         .directive('bbDataMartResponsiveDashboard', ['bbDataMartReportService', function (bbDataMartReportService) {
             return {
